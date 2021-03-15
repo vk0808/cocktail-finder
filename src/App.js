4be63e61ac9686624c5da02669db61cb04921fdf
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-03-15 19:10:56 +0530

    included pages using router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import "./styles.css";
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SingleCocktail from "./pages/SingleCocktail";
+import Error from "./pages/Error";
 
 export default function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-    </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/cocktail/:id" children={<SingleCocktail />}></Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
