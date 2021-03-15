90395415813cc97842b34cf5702625e75a7af2fe
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-03-15 19:17:10 +0530

    added navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,23 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <>
-      <h1>navbar</h1>
-    </>
+    <nav className="navbar">
+      <div className="nav-center">
+        <Link to="/">
+          <h1 className="logo">Cocktail Finder</h1>
+        </Link>
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
