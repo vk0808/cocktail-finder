5569b01676f057444eeb949d2e3b9b455935e264
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-03-15 18:44:37 +0530

    added Cocktail and List component

---
+++ src/components/Cocktail.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Cocktail = () => {
+  return (
+    <div>
+      <h2>cocktail component</h2>
+    </div>
+  );
+};
+
+export default Cocktail;
---
+++ src/components/CocktailList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Cocktail from "./Cocktail";
+import Loading from "./Loading";
+
+const CocktailList = () => {
+  return (
+    <div>
+      <h2>cocktail list component</h2>
+    </div>
+  );
+};
+
+export default CocktailList;
