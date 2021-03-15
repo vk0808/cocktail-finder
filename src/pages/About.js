416ff5169b83765a208fa183fb7742557be0d445
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-03-15 19:27:14 +0530

    filled about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,32 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="about-section section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. At auctor urna nunc
+        id cursus metus aliquam. At imperdiet dui accumsan sit amet nulla
+        facilisi. Porttitor eget dolor morbi non. Suspendisse ultrices gravida
+        dictum fusce ut placerat orci nulla pellentesque. Tincidunt praesent
+        semper feugiat nibh sed. Bibendum enim facilisis gravida neque convallis
+        a. Tellus integer feugiat scelerisque varius morbi enim. Massa sed
+        elementum tempus egestas sed sed risus. Elementum pulvinar etiam non
+        quam.
+      </p>
+      <p>
+        Arcu odio ut sem nulla pharetra diam sit amet. Pulvinar elementum
+        integer enim neque volutpat ac. Et egestas quis ipsum suspendisse
+        ultrices gravida. Diam donec adipiscing tristique risus nec. Magna eget
+        est lorem ipsum dolor sit. Ornare aenean euismod elementum nisi quis
+        eleifend quam. Morbi leo urna molestie at elementum eu facilisis sed
+        odio. Id volutpat lacus laoreet non curabitur gravida arcu. Egestas diam
+        in arcu cursus. Est pellentesque elit ullamcorper dignissim cras
+        tincidunt. Enim blandit volutpat maecenas volutpat. Arcu bibendum at
+        varius vel pharetra vel turpis. Augue lacus viverra vitae congue eu.
+        Elit eget gravida cum sociis natoque penatibus et magnis dis.
+      </p>
+    </section>
   );
 };
 
