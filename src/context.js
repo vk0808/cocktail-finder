8ccce63c1914eb343c1dd110043a70bd35444fc6
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-03-16 18:42:43 +0530

    add callback hook

--- src/context.js
+++ src/context.js
@@ -10,7 +10,7 @@ export const AppProvider = ({ children }) => {
   const [searchTerm, setSearchTerm] = useState("a");
   const [cocktails, setCocktails] = useState([]);
 
-  const fetchDrinks = async () => {
+  const fetchDrinks = useCallback(async () => {
     setLoading(true);
     try {
       const response = await fetch(`${url}${searchTerm}`);
@@ -35,7 +35,7 @@ export const AppProvider = ({ children }) => {
       console.log(error);
       setLoading(false)
     }
-  };
+  }, [searchTerm]);
 
   useEffect(()=> {
     fetchDrinks()
