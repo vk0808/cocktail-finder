e01ab672cda58cae46c44326aafdf47ff34b5935
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-03-16 18:39:12 +0530

    no change

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -5,11 +5,11 @@ import Loader from "react-loader-spinner";
 const Loading = () => {
   return (
     <div className="loader">
-      <Loader
-        type="TailSpin"
-        color="#476a2e"
-        height={80}
-        width={80}
+      <Loader 
+        type="TailSpin" 
+        color="#476a2e" 
+        height={80} 
+        width={80} 
       />
     </div>
   );
