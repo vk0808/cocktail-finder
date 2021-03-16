39547b45ebfacf21a8eef6cba84da42b39124ec6
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-03-16 14:20:23 +0530

    fetch cocktail data

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -7,7 +7,11 @@ const CocktailList = () => {
   const { loading, cocktails } = useGlobalContext();
 
   if (loading) {
-    return <Loading />
+    return <Loading />;
+  }
+
+  if (cocktails.length < 1) {
+    return <h2 className="section-title">no cocktails matched your search criteria</h2>;
   }
 
   return (
